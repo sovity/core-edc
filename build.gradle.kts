08c653f028b17daf79ea31e19cab3c5f74e0b58d
Christophe Loiseau <christophe.loiseau@sovity.de>
2025-02-05 08:44:38 +0100

    prepare releasing

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@
 
 plugins {
     `java-library`
+    `maven-publish`
 }
 
 val javaVersion: String by project
@@ -56,5 +57,30 @@ allprojects {
         configFile = rootProject.file("resources/edc-checkstyle-config.xml")
         configDirectory.set(rootProject.file("resources"))
     }
+}
 
+subprojects {
+    apply(plugin = "maven-publish")
+
+    publishing {
+        repositories {
+            maven {
+                name = "AzureTestPackages"
+                url = uri("https://pkgs.dev.azure.com/sovity/Test/_packaging/test/maven/v1")
+                credentials {
+                    username = "sovity"
+                    password = project.findProperty("azure.token") as String? ?: System.getenv("AZURE_TOKEN")
+                }
+            }
+
+            maven {
+                name = "AzurePackages"
+                url = uri("https://pkgs.dev.azure.com/sovity/41799556-91c8-4df6-8ddb-4471d6f15953/_packaging/core-edc/maven/v1")
+                credentials {
+                    username = "sovity"
+                    password = project.findProperty("azure.token") as String? ?: System.getenv("AZURE_TOKEN")
+                }
+            }
+        }
+    }
 }
