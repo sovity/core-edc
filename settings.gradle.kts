79ee8e4f26f9912f060807a7d0d7ac640817afad
Christophe Loiseau <christophe.loiseau@sovity.de>
2025-04-23 18:36:00 +0200

    Add plugin repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,6 +27,23 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven {
+            name = "AzureTest"
+            url = uri("https://pkgs.dev.azure.com/sovity/Test/_packaging/test/maven/v1")
+            credentials {
+                username = "sovity"
+                password = providers.gradleProperty("azure.token") as String? ?: System.getenv("AZURE_TOKEN")
+            }
+        }
+
+        maven {
+            name = "Azure"
+            url = uri("https://pkgs.dev.azure.com/sovity/41799556-91c8-4df6-8ddb-4471d6f15953/_packaging/core-edc/maven/v1")
+            credentials {
+                username = "sovity"
+                password = providers.gradleProperty("azure.token") as String? ?: System.getenv("AZURE_TOKEN")
+            }
+        }
     }
 }
 
