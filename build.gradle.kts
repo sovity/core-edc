86b8cb33703d5a3097140b91ba1a0d9e8d8feb55
Christophe Loiseau <christophe.loiseau@sovity.de>
2025-04-24 10:12:21 +0200

    more!

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,6 +93,29 @@ allprojects {
     }
 }
 
+subprojects {
+    repositories {
+        maven {
+            name = "AzureTest"
+            url = uri("https://pkgs.dev.azure.com/sovity/Test/_packaging/test/maven/v1")
+            credentials {
+                username = "sovity"
+                password = project.findProperty("azure.token") as String? ?: System.getenv("AZURE_TOKEN")
+            }
+        }
+
+        maven {
+            name = "Azure"
+            url =
+                uri("https://pkgs.dev.azure.com/sovity/41799556-91c8-4df6-8ddb-4471d6f15953/_packaging/core-edc/maven/v1")
+            credentials {
+                username = "sovity"
+                password = project.findProperty("azure.token") as String? ?: System.getenv("AZURE_TOKEN")
+            }
+        }
+    }
+}
+
 repositories {
     maven {
         name = "AzureTest"
